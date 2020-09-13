b59eed70a0acc1d7f0cb8583dde7ea08b0500a25
Subin Siby <mail@subinsb.com>
2020-09-14 01:49:47 +0530

    Made it easy to include editor in site

---
+++ build-editor.sh
@@ -0,0 +1,10 @@
+cd varnam-editor
+npm install
+
+export VUE_PUBLIC_PATH="/editor/"
+npm run build
+
+sed -i 's+http://127.0.0.1:8089+https://api.varnamproject.com+g' dist/js/*.js
+
+rm -rf $(realpath ../static/)/editor
+mv dist $(realpath ../static/)/editor
