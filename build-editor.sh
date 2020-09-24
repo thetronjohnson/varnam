7446f46ac1a9950630c3510d4b4d64825692c315
Subin Siby <mail@subinsb.com>
2020-09-25 05:17:57 +0530

    Update editor, build script updated to accomodate $VARNAM_OFFLINE

--- build-editor.sh
+++ build-editor.sh
@@ -4,7 +4,10 @@ npm install
 export VUE_PUBLIC_PATH="/editor/"
 npm run build
 
-sed -i 's+http://127.0.0.1:8089+https://api.varnamproject.com+g' dist/js/*.js
+sed -i 's+http://127.0.0.1:8123+https://api.varnamproject.com+g' dist/js/*.js
+
+# From offline=true to offline=false
+sed -i 's+$VARNAM_OFFLINE=!0+$VARNAM_OFFLINE=!1+g' dist/js/*.js
 
 rm -rf $(realpath ../static/)/editor
 mv dist $(realpath ../static/)/editor
