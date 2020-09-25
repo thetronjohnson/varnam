bbbe25baeb5543189bd0acf79d1b478736479707
Subin Siby <mail@subinsb.com>
2020-09-25 21:12:21 +0530

    Add varnam review backend

--- build-editor.sh
+++ build-editor.sh
@@ -5,6 +5,7 @@ export VUE_PUBLIC_PATH="/editor/"
 npm run build
 
 sed -i 's+http://127.0.0.1:8123+https://api.varnamproject.com+g' dist/js/*.js
+sed -i 's+http://127.0.0.1:3000+https://kde.smc.org.in:8124+g' dist/js/*.js
 
 # From offline=true to offline=false
 sed -i 's+$VARNAM_OFFLINE=!0+$VARNAM_OFFLINE=!1+g' dist/js/*.js
